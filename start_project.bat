1dcc9ee5f36a00ea36edb27d3f3328e64ae26f91
plhernans <plhernans@gmail.com>
2024-04-28 02:08:46 +0200

    new_file_sh

--- start_project.bat
+++ start_project.bat
@@ -1,4 +1,4 @@
 start cmd.exe /k "cd C:\Program Files\MongoDB\Server\5.0\bin && mongod.exe --dbpath C:/data/db"
 start cmd.exe /k "cd D:\Workspace\www\desarrollo\proyecto_redsocial_nodejs && npm start"
 
-brew services start mongodb/brew/mongodb-community@6.0
+
